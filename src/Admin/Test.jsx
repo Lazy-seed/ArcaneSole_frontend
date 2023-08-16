61121b4242da792f829dcd85f930ba49e75840ad
Aryan <aryan34.model@gmail.com>
2023-08-16 18:08:18 +0530

    stared

---
+++ src/Admin/Test.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Test() {
+    return (
+            <div className='container-sm'>
+            <div class="row g-3 align-items-center m-3">
+                <label for="exampleFormControlInput1" class="col-auto">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+            <div class="row g-3 align-items-center m-3 ">
+                <label for="exampleFormControlTextarea1" class="col-auto">Example textarea</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+
+            </div>
+        </div>
+    )
+}
